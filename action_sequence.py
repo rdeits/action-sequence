e9fe7b8d6b4eaf8fdc7ce0c018876d53330fe7af
Robin Deits <robin.deits@gmail.com>
2014-10-01 17:24:31 -0400

    first draft

---
+++ action_sequence.py
@@ -0,0 +1,30 @@
+from __future__ import division
+
+import numpy as np
+import PIL.Image as Im
+import os
+import sys
+
+"""
+Take a set of images of some action sequence and combine them into a single image showing all of the poses of the moving body. We do this by finding the median image of all the frames, then extracting only the pixels which do *not* match the median image from each source file.
+"""
+
+folder = sys.argv[1]
+source_fnames = reversed(sorted(os.listdir(folder)))
+images = []
+for f in source_fnames:
+    img = Im.open(os.path.join(folder, f))
+    images.append(np.expand_dims(np.array(img), 3))
+
+
+stack = np.concatenate(images, axis=3)
+
+med_img = np.array(np.median(stack, axis=3), dtype=np.uint8)
+
+out_img = med_img.copy()
+for img in images:
+    img = np.squeeze(img)
+    mask = img != med_img
+    out_img[mask] = img[mask]
+out_img = Im.fromarray(out_img)
+out_img.save(os.path.join(folder, 'out.png'))
